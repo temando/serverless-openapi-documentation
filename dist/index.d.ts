b9412877607bc459fd519a6a52ab2f768163e5c9
Kevin Zhou <kevin.zhou@gmi-mr.com>
2019-04-28 22:23:14 -0700

    remove dist folder, and change package.json to point to dist/

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { ServerlessOpenApiDocumentation } from './ServerlessOpenApiDocumentation';
-export default ServerlessOpenApiDocumentation;
--- dist/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { IMerge } from 'lutils';
-export declare const merge: IMerge;
-export declare const clone: <S>(source: S) => S;
-export declare function isIterable(obj: any): boolean;
